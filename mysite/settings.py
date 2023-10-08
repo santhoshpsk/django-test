811a4b624af2788bc5d3ec1991f2f60075a00053
santhoshpsk <santhoshpsk@outlook.com>
2023-10-08 11:10:52 +0000

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '1234',
+        'HOST': '172.17.0.2',
+        'PORT': '5432',
     }
 }
 
