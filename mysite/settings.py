22bdabaf496743f0d799cf915723b861104adb44
santhoshpsk <santhoshpsk@outlook.com>
2023-10-08 12:06:31 +0000

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://" + os.getenv("domain_name"), "https://" + os.getenv("domain_name")]
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
